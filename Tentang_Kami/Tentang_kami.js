0f7212168f33de25da489423d80f92e6d000d0df
ARYUD <yudhaarya637@gmail.com>
2025-12-22 02:36:59 +0700

    Upload ulang Tentang Kami sebagai folder biasa

---
+++ Tentang_Kami/Tentang_kami.js
@@ -0,0 +1,60 @@
+const nav = document.querySelector('nav');
+const links = document.querySelector('.header-links');
+const navOffsettop =  nav.offsetTop;
+const navMobile = document.querySelector('.mobile-nav');
+const mobilenCon = document.querySelector('.mobile-menu-container')
+
+const body = document.body
+
+window.addEventListener('scroll', () => {
+    if(window.pageYOffset > navOffsettop){
+        nav.classList.add('sticky');
+        navMobile.classList.add('stickyMobile');
+        mobilenCon.classList.add('stickyMobileContainer');
+        body.style.marginTop = "100px";
+        links.style.opacity = '0';
+    }
+    else{
+        nav.classList.remove('sticky');
+        navMobile.classList.remove('stickyMobile');
+        mobilenCon.classList.remove('stickyMobileContainer');
+        body.style.marginTop = "0";
+        links.style.opacity = '100';
+    }
+})
+
+// mobile menu icon
+const menuIcon = document.querySelector('.menu-icon');
+const closeIcon = document.querySelector('.cross-icon');
+const mobileMenuContainer = document.querySelector ('.mobile-menu-container');
+let mobileMenuContainerSticky = document.querySelector('.mobile-menu-container.stickyMobileContainer')
+
+menuIcon.addEventListener('click', () => {
+    mobileMenuContainer.classList.add('active');
+    menuIcon.classList.add('erase');
+    mobileMenuContainerSticky.classList.add('active');
+
+})
+
+closeIcon.addEventListener('click', () => {
+    mobileMenuContainer.classList.remove('active');
+    menuIcon.classList.remove('erase');
+    mobileMenuContainerSticky.classList.add('active');
+})
+
+// scroll down animation
+const observerOptions = {
+    threshold: 0.4
+};
+
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add('aktif');
+            observer.unobserve(entry.target); 
+        } 
+    });
+}, observerOptions);
+
+const elemenAnimasi = document.querySelectorAll('.scroll-anim');
+elemenAnimasi.forEach((el) => observer.observe(el));
