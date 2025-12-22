360c465bcb4ff82989d0999b3183df4080e0022b
ARYUD <yudhaarya637@gmail.com>
2025-12-22 14:14:43 +0700

    Penyesuian UI untuk Ipad ukurna 800px

--- Tentang_Kami/Tentang_kami.js
+++ Tentang_Kami/Tentang_kami.js
@@ -61,9 +61,10 @@ elemenAnimasi.forEach((el) => observer.observe(el));
 
 // Hover untuk core-service
 const coreService = document.querySelectorAll('.servicebox');
-const isMobile = window.matchMedia("(max-width: 1100px)").matches;
+const isMobilePad = window.matchMedia("(max-width: 1100px)").matches;
+const isMobilePadMini = window.matchMedia("(max-width: 800px)").matches;
 
-if(isMobile){
+if(isMobilePad){
     coreService.forEach((kotakCore) => {
         kotakCore.classList.remove('core-aktif');
     })
@@ -84,7 +85,26 @@ else{
 //Hover untuk core value
 const coreValue = document.querySelectorAll('.kotak');
 
-if(isMobile){
+if(isMobilePadMini){
+    const observerOptions = {
+        threshold: 0.5,
+        rootMargin: "-400px 0px -500px 0px"
+    };
+
+    const observer = new IntersectionObserver((entries) => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting){
+                entry.target.classList.add('core-value-aktif');
+            } else {
+                entry.target.classList.remove('core-value-aktif')
+            }
+        });
+    }, observerOptions);
+
+    coreValue.forEach((kotakValue) => observer.observe(kotakValue));
+}
+
+else if(isMobilePad){
     const observerOptions = {
         threshold: 0.5,
         rootMargin: "-500px 0px -700px 0px"
@@ -102,6 +122,7 @@ if(isMobile){
 
     coreValue.forEach((kotakValue) => observer.observe(kotakValue));
 }
+
 else{
     coreValue.forEach((kotakValue) => {
     kotakValue.addEventListener('mouseenter', () =>{
